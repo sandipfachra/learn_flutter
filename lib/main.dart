0290bd4a521530aa89204cef68679bbe8e3a6f9e
Sandip Fachara <sandip.fachara35@gmail.com>
2021-08-11 16:38:43 +0530

    theme extrected

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery/home_page.dart';
+import 'package:food_delivery/pages/home_page.dart';
 import 'package:food_delivery/pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:food_delivery/utils/routes.dart';
+import 'package:food_delivery/widgets/themes.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,19 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-        fontFamily: GoogleFonts.poppins().fontFamily,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-      // initialRoute: "/home",
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
